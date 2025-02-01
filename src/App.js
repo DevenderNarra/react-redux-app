775d0761df75fddf8df259d04823b5a41395d4ad
DevenderNarra <devendernarra@gmail.com>
2025-02-01 19:48:19 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './containers/Header';
+import ProductListing from './containers/ProductListing';
+import ProductDetails from './containers/ProductDetails';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<ProductListing />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          {/* Corrected 404 Route */}
+          <Route path="*" element={<h2>404 - Page Not Found</h2>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
